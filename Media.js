93bf88202d72876aaeb3e176be5dee1762418af1
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-03-14 19:29:31 -0700

    Se agregaron un método más, raizEnesima

--- Media.js
+++ Media.js
@@ -1,14 +1,32 @@
 function mediaAritmetica () {
 
-    let prima = [19, 18, 13, 11];
-    let veta = 0;
-    for(let i = prima.length -1 ; i >= 0; i--) {
-        veta += prima[i];
-        console.log(prima[i]);
-        
-    }
-    veta = veta/prima.length;
-    console.log(veta);
+  let arregloNumeros = [19, 18, 13, 11];
+  let suma = 0;
+  for(let i = arregloNumeros.length -1 ; i >= 0; i--) {
+    suma += arregloNumeros[i];
+    console.log(arregloNumeros[i]);
+      
+  }
+  suma = suma/arregloNumeros.length;
+  console.log(suma);
 }
 
 mediaAritmetica();
+
+function raizEnesima (){
+
+  let alfa = 12;
+  let enesima = 2;
+  console.log(Math.pow(alfa, 1/enesima));
+}
+
+raizEnesima();
+
+function mediaGeometrica(){
+  let arregloMediaGeo = [19, 18, 13, 11];
+  let total = 0;
+  for (let y = arregloMediaGeo.length -1; t >= 0; y++){
+    total *= arregloMediaGeo[y]; 
+  }
+
+}
