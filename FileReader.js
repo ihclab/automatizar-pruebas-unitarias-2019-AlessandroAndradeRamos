725eb92a74fbd4ac7c321149592c9792357ad91c
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-03-19 12:11:07 -0700

    Primera iteración del Problema03

--- FileReader.js
+++ FileReader.js
@@ -1,8 +1,16 @@
-const filesystem = require ('fs');
-filesystem.readFile('./CasosPrueba.txt', 'utf-8', (err, data) => {
-    if(err)
-        throw new Error(err);
-    
-    let barras = data.split('\r\n');
-    console.log(barras);
-})
+const fs = require ('fs');
+class FileReader{    
+    leer () {
+        fs.readFile('./CasosPrueba.txt', 'utf-8', (err, data) => {
+            if(err)
+                throw new Error(err);
+            
+            let barras = data.split('\r\n');
+            console.log(barras);
+        })   
+    }
+}
+
+let Lector = new FileReader();
+Lector.leer();
+
