35784539583be294cc8f34a0a486424823de3f1a
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-03-19 09:28:37 -0700

    Se terminaron los metodos y se hizo la conversion a clase pero ahora si sin mentiras

--- Media.js
+++ Media.js
@@ -1,40 +1,5 @@
-// function mediaAritmetica() {
-
-//   let arregloNumeros = [19, 18, 13, 11];
-//   let suma = 0;
-//   for (let i = arregloNumeros.length - 1; i >= 0; i--) {
-//     suma += arregloNumeros[i];
-//     console.log(arregloNumeros[i]);
-
-//   }
-//   suma = suma / arregloNumeros.length;
-//   console.log(suma);
-// }
-
-// mediaAritmetica();
-
-// function raizEnesima() {
-
-//   let alfa = 12;
-//   let enesima = 2;
-//   console.log(Math.pow(alfa, 1 / enesima));
-// }
-
-// raizEnesima();
-
-// function mediaGeometrica() {
-//   let arregloMediaGeo = [19, 18, 13, 11];
-//   let total = 0;
-//   for (let y = arregloMediaGeo.length - 1; t >= 0; y++) {
-//     total *= arregloMediaGeo[y];
-//   }
-
-// }
-
-// Otra manera
-
-function Media() {
-  this.mediaAritmetica = function (arregloNumeros) {
+class Media {
+  mediaAritmetica (arregloNumeros) {
     let suma = 0;
     for (let i = arregloNumeros.length - 1; i >= 0; i--) {
       suma += arregloNumeros[i];
@@ -42,11 +7,11 @@ function Media() {
     suma = suma / arregloNumeros.length;
     return suma;
   }
-  this.raizEnesima = function (alfa, omega) {
+  raizEnesima (alfa, omega) {
     let result = Math.pow(alfa, 1 / omega);
     return result;
   }
-  this.mediaGeometrica = function (arregloMediaGeo) {
+  mediaGeometrica (arregloMediaGeo) {
     let total = 1;
     for (let y = arregloMediaGeo.length - 1; y >= 0; y--) {
       total *= arregloMediaGeo[y];
@@ -54,7 +19,7 @@ function Media() {
     let result = this.raizEnesima(total, arregloMediaGeo.length)
     return result;
   }
-  this.mediaArmonica = function (arregloMediaArmo) {
+  mediaArmonica (arregloMediaArmo) {
     let toal = 0;
     for (let i = arregloMediaArmo.length - 1; i >= 0; i--) {
       toal += 1/arregloMediaArmo[i];
