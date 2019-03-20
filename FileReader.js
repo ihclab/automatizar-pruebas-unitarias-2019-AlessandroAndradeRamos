b60efd89f3ba215757d725230a8438779dec5fb9
AlessandroAndradeRamos <aandrade8@ucol.mx>
2019-03-19 21:07:14 -0600

    Primera iteración del Problema04

--- FileReader.js
+++ FileReader.js
@@ -7,7 +7,16 @@ class FileReader{
             
             let barras = data.split('\r\n');
             console.log(barras);
-        })   
+            for (let i = barras.length - 1; i >= 0; i--){
+                barras[i] = barras[i].split(':');
+            }
+            console.log(barras);
+            for (let e = 0; e <= barras.length - 1; e++){
+                barras[e][2] = parseFloat(barras[e][2]);
+                barras[e][3] = Number(barras[e][3]);
+            }
+            console.log(barras);
+        })
     }
 }
 
